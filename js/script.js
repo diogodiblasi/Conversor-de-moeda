8cc72c36c99088d5cb18ba4e635fc51c0b8213b4
diogodiblasi <diogodiblasi@gmail.com>
2023-03-09 23:31:32 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+const dropList = document.querySelectorAll("form select"),
+fromCurrency = document.querySelector(".from select"),
+toCurrency = document.querySelector(".to select"),
+getButton = document.querySelector("form button");
+
+let country_list = {
+
+    "USD" : "US", 
+    "EUR" : "FR", 
+    "GBP" : "GB", 
+    "JPY" : "JP",
+    "CNY" : "CN",  
+}
+
+for (let i = 0; i < dropList.length; i++) {
+    for(let currency_code in country_list){
+        let selected = i == 0 ? currency_code == "USD" ? "selected" : "" : currency_code == "EUR" ? "selected" : "";
+        let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
+        dropList[i].insertAdjacentHTML("beforeend", optionTag);
+    }
+}
+window.addEventListener("load", ()=>{
+    getExchangeRate();
+});
+getButton.addEventListener("click", e =>{
+    e.preventDefault();
+    getExchangeRate();
+});
+
+function getExchangeRate(){
+    const amount = document.querySelector("form input");
+    const exchangeRateTxt = document.querySelector("form .exchange-rate");
+    let amountVal = amount.value;
+    if(amountVal == "" || amountVal == "0"){
+        amount.value = "1";
+        amountVal = 1;
+    }
+    exchangeRateTxt.innerText = "Convertendo valor";
+    let url = `https://v6.exchangerate-api.com/v6/b1a761c7f827dbdcb66f2b7f/latest/${fromCurrency.value}`;
+    fetch(url).then(response => response.json()).then(result =>{
+        let exchangeRate = result.conversion_rates[toCurrency.value];
+        let totalExRate = (amountVal * exchangeRate).toFixed(2);
+        exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExRate} ${toCurrency.value}`;
+    }).catch(() =>{
+        exchangeRateTxt.innerText = "Não foi possível fazer a conversão";
+    });
+}
